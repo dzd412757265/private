b6b731d8cf1b2f17c9e8c6fba76d443356804460
YuanqingGu <dzd412757265@users.noreply.github.com>
2022-10-19 14:52:09 +0800

    Update healthRes.js

--- healthRes.js
+++ healthRes.js
@@ -1,4 +1,14 @@
+var jkbTestTime = {
+  "errcode": "BIS:10200",
+	"errmsg": null,
+	"data": {
+		"cacheDay": "20221017",
+		"detResult": "阴性",
+		"afterTestTime": "0"
+	}
+};
 
+$done({body: JSON.stringify(jkbTestTime)});
 
 
 
